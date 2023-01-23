75dfcf92fea1e70d7368d0b16bd5dced2da3c923
Harrison Bishop <harrisonbishsp@gmail.com>
2023-01-23 15:41:19 +0000

    fetched data for london weather and added very basic styling to page, with background image

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 fetch(
-  "http://api.openweathermap.org/data/2.5/weather?q={London}&appid={f2702338122078f9274942735eac40e4}",
+  "http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=01140b4c9927771a31d8304a92387fdb",
   { mode: "cors" }
-).then(function (response) {
-  console.log(response.json());
-});
+).then((response) => response.json().then((data) => console.log(data)));
+
 
 // create dropdown that has search list of cities. Create page of wather based off cirty selected
