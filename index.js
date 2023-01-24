f5b525e4e664931efc5305ed5619573c73d4b2d0
Harrison Bishop <harrisonbishsp@gmail.com>
2023-01-24 10:21:12 +0000

    created function to do the fetch request, and module with functions to grab the form data

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-fetch(
-  "http://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=01140b4c9927771a31d8304a92387fdb",
-  { mode: "cors" }
-).then((response) => response.json().then((data) => console.log(data)));
+// Object Containing Location
+locations = {
+  place: "London",
+};
 
+console.log(locations);
 
-// create dropdown that has search list of cities. Create page of wather based off cirty selected
+// Fetch request for weather Data
+fetchRequest = function () {
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${locations.place},uk&appid=01140b4c9927771a31d8304a92387fdb`,
+    { mode: "cors" }
+  ).then((response) => response.json().then((data) => console.log(data)));
+};
+fetchRequest();
+
+const getInputData = {
+  grabInputData: function (e) {
+    e.preventDefault();
+
+    const chosenLocation = document.getElementById("search-bar").value;
+    console.log(chosenLocation);
+
+    locations.place = chosenLocation;
+    document.forms[0].reset();
+    fetchRequest();
+    return this.location;
+  },
+
+  attachEventListener: function () {
+    document.addEventListener("DOMContentLoaded", () => {
+      document
+        .getElementById("submit")
+        .addEventListener("click", this.grabInputData);
+    });
+  },
+};
+getInputData.attachEventListener();
