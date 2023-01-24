65f79e3ab15dea9045965458e0759dbfa1fbe6e1
Harrison Bishop <harrisonbishsp@gmail.com>
2023-01-24 10:44:50 +0000

    added notes

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 locations = {
   place: "London",
 };
-
 console.log(locations);
 
 // Fetch request for weather Data
-fetchRequest = function () {
+const fetchRequest = function () {
   fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${locations.place},uk&appid=01140b4c9927771a31d8304a92387fdb`,
     { mode: "cors" }
@@ -14,6 +13,7 @@ fetchRequest = function () {
 };
 fetchRequest();
 
+// grabs input Data and changes the locations.place
 const getInputData = {
   grabInputData: function (e) {
     e.preventDefault();
@@ -36,3 +36,5 @@ const getInputData = {
   },
 };
 getInputData.attachEventListener();
+
+
